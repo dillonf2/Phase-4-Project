85b3db358080ab3f233678a18c7400fa580f4082
Frank Dillon <dillonf2@outlook.com>
2023-12-09 21:08:46 -0500

    adjusted margins and formatting slightly

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,70 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Projects from "./Projects";
+import Navbar from "./Navbar";
+import Login from "./Login";
+import Signup from "./Signup";
+import MyReviews from "./MyReviews";
+import SubmitProject from "./SubmitProject";
+import ClaimOwnership from "./ClaimOwnership";
+
 
 function App() {
-  return <h1>Project Client</h1>;
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (userData) => {
+    setUser(userData);
+    setLoggedIn(true);
+    alert("Login Successful!");
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+    setLoggedIn(false)
+    alert("Logout Successful!")
+  };
+
+  return (
+    <div>
+      <Navbar isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
+
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route exact path="/">
+          <Home user={user} />
+        </Route>
+        <Route path="/login">
+          <Login handleLogin={handleLogin} />
+        </Route>
+        {isLoggedIn && (
+          <Route path="/my-reviews">
+            <MyReviews user={user}/>
+          </Route>
+        )}
+        {isLoggedIn && (
+          <Route path="/submit-project">
+            <SubmitProject user={user}/>
+          </Route>
+        )}  
+        {isLoggedIn && (
+          <Route path="/claim-ownership">
+            <ClaimOwnership user={user}/>
+          </Route>
+        )}                  
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
